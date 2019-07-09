4db582da3062ffee3e9b81f402cdaa417dae6285
Rajeet <rajeetdel@gmail.com>
2019-07-09 19:48:11 +0530

    Updates

---
+++ precache-manifest.3fdf5a9985c36cb2f64d2df7806c6226.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b3fc126936bac18bcde7db5d9d56a67",
+    "url": "/snakegameapp/index.html"
+  },
+  {
+    "revision": "c3195dde5425b58e7d9f",
+    "url": "/snakegameapp/static/css/main.263ec59e.chunk.css"
+  },
+  {
+    "revision": "b42aa7dd048f4b5d252a",
+    "url": "/snakegameapp/static/js/2.5c4e487f.chunk.js"
+  },
+  {
+    "revision": "c3195dde5425b58e7d9f",
+    "url": "/snakegameapp/static/js/main.5fbc1da6.chunk.js"
+  },
+  {
+    "revision": "5ea78c361b9db70110d7",
+    "url": "/snakegameapp/static/js/runtime~main.04acf8de.js"
+  }
+]);
